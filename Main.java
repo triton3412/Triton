325bd4ca8b00d8573c31ee8d41019750eb8c854f
triton <triton3412@gmail.com>
2024-06-25 09:12:10 +0900

    백준 풀이 1065번 완성

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
-import javax.naming.PartialResultException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static int COUNT = 0;
-    public static ArrayList<Integer> list = new ArrayList<Integer>();
     public static int num(int n){
         int count = 0;
         while(n >= 1){
@@ -14,18 +10,9 @@ public class Main {
         }
         return count;
     } //숫자 자릿수 계산하는 함수
-    public static int toint(int [] n){
-        int num = 0;
-        for (int nas : n) {
-            num *= 10;
-            num += nas;
-        }
-        return num;
-    }//정수배열을 정수로 만드는 함수
 
     public static boolean check(int n){
         boolean found = true;
-        int thi = n;
         if (n < 100){
             return true;
         }
@@ -43,9 +30,6 @@ public class Main {
                 }
             }
         }
-        if (found){
-            list.add(thi);
-        }
         return found;
     }
 
@@ -57,7 +41,6 @@ public class Main {
                 COUNT++;
             }
         }
-        System.out.println(list);
         System.out.println(COUNT);
     }
 }
